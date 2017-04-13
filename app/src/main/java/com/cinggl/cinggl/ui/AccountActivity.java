07dcc69ec89d6bbf58ae2af0bb526881b6b8e062
jo-ngei <johnngei2@gmail.com>
2017-04-13 18:01:35 +0300

    google signin

--- app/src/main/java/com/cinggl/cinggl/ui/AccountActivity.java
+++ app/src/main/java/com/cinggl/cinggl/ui/AccountActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.cinggl.cinggl.R;
 
-public class SingUpWithActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_up_with);
+        setContentView(R.layout.activity_account);
     }
 }
--- app/src/main/java/com/cinggl/cinggl/ui/SingUpActivity.java
+++ app/src/main/java/com/cinggl/cinggl/ui/SingUpActivity.java
@@ -10,6 +10,6 @@ public class SingUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_up);
+        setContentView(R.layout.activity_sign_up);
     }
 }
